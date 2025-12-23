a785637076b91857876b0575508c970a6116174e
angelaltuve <Ronstadt@murena.io>
2025-12-22 20:35:18 -0400

    The code was formatted.

--- .config/nvim/lua/config/conform.lua
+++ .config/nvim/lua/config/conform.lua
@@ -1,34 +1,34 @@
 return {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		java = { "google-java-format" },
-		c = { "clang-format" },
-		cpp = { "clang-format" },
-		python = { "isort", "black" },
-		toml = { "taplo" },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    java = { "google-java-format" },
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+    python = { "isort", "black" },
+    toml = { "taplo" },
 
-		-- webdev
-		javascript = { "biome" },
-		javascriptreact = { "biome" },
-		typescript = { "biome" },
-		typescriptreact = { "biome" },
+    -- webdev
+    javascript = { "biome" },
+    javascriptreact = { "biome" },
+    typescript = { "biome" },
+    typescriptreact = { "biome" },
 
-		css = { "biome" },
-		html = { "biome" },
-		json = { "biome" },
-		jsonc = { "biome" },
-		svelte = { "deno_fmt" },
-		sh = { "shfmt" },
-		yaml = { "yamlfmt" },
+    css = { "biome" },
+    html = { "biome" },
+    json = { "biome" },
+    jsonc = { "biome" },
+    svelte = { "deno_fmt" },
+    sh = { "shfmt" },
+    yaml = { "yamlfmt" },
 
-		-- Databases
-		sql = { "sqlfluff" },
-		mysql = { "sqlfluff" },
-		plsql = { "sqlfluff" },
+    -- Databases
+    sql = { "sqlfluff" },
+    mysql = { "sqlfluff" },
+    plsql = { "sqlfluff" },
 
-		-- Documentation
-		markdown = { "prettier" },
-		latex = { "latexindent" },
-		tex = { "latexindent" },
-	},
+    -- Documentation
+    markdown = { "prettier" },
+    latex = { "latexindent" },
+    tex = { "latexindent" },
+  },
 }
