a56e6c2d00a4c03c6214caccfd06489060ed7ea9
angelaltuve <Ronstadt@murena.io>
2025-12-23 23:01:06 -0400

    add supermaven source and minor changes

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -3,75 +3,62 @@ local lspkind = require("lspkind")
 local luasnip = require("luasnip")
 
 return {
-	completion = {
-		completeopt = "menu,menuone,noinsert",
-	},
+  completion = {
+    completeopt = "menu,menuone,noinsert,noselect",
+  },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    }),
 
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-
-	mapping = cmp.mapping.preset.insert({
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = false,
-		}),
-
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				if not cmp.get_selected_entry() then
-					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-				end
-				cmp.confirm()
-			elseif luasnip.expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp", keyword_length = 1 },
-		{ name = "luasnip", keyword_length = 2 },
-		{ name = "buffer", keyword_length = 3 },
-		{ name = "async_path" },
-		{ name = "nvim_lua" },
-		{ name = "lazydev", group_index = 0 },
-	}),
-
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = "symbol_text",
-			maxwidth = 50,
-			ellipsis_char = "...",
-		}),
-	},
-
-	experimental = {
-		ghost_text = true,
-	},
-
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "supermaven", priority = 1000, keyword_length = 1 },
+    { name = "nvim_lsp", trigger_characters = { "-" }, priority = 900, keyword_length = 2 },
+    { name = "luasnip", priority = 800, keyword_length = 3 },
+    { name = "async_path", priority = 700 },
+    { name = "buffer", priority = 500, keyword_length = 4 },
+    { name = "nvim_lua", priority = 400 },
+    { name = "lazydev", priority = 300, group_index = 0 },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({mode = "symbol_text"}),
+  },
+  experimental = {
+    ghost_text = true,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
 }
