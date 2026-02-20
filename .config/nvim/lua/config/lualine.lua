4d5ce052e2fcd4339ff2f654b70a76cfcabd3cd3
angelaltuve <Ronstadt@murena.io>
2026-02-20 10:29:34 -0400

    formatting

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,17 +1,17 @@
 return {
-	options = {
-		theme = "auto",
-		component_separators = "",
-		section_separators = "",
-		globalstatus = true,
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = { "diagnostics", "diff" },
-		lualine_x = { "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	extensions = { "nvim-tree", "lazy", "man", "nvim-dap-ui" },
+  options = {
+    theme = "auto",
+    component_separators = "",
+    section_separators = "",
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch" },
+    lualine_c = { "diagnostics", "diff" },
+    lualine_x = { "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  extensions = { "nvim-tree", "lazy", "man", "nvim-dap-ui" },
 }
