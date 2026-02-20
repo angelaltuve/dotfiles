5033b546c2440b133395804a989ee1d6eb7201d0
angelaltuve <Ronstadt@murena.io>
2026-02-20 10:28:53 -0400

    formatting

--- .config/nvim/lua/config/mason.lua
+++ .config/nvim/lua/config/mason.lua
@@ -1,13 +1,42 @@
 return {
-	PATH = "skip",
+  PATH = "skip",
 
-	ui = {
-		icons = {
-			package_pending = " ",
-			package_installed = " ",
-			package_uninstalled = " ",
-		},
-	},
+  ui = {
+    icons = {
+      package_pending = " ",
+      package_installed = " ",
+      package_uninstalled = " ",
+    },
+  },
 
-	max_concurrent_installers = 10,
+  max_concurrent_installers = 10,
+
+  ensure_installed = {
+    "bash-language-server",
+    "black",
+    "clangd",
+    "clang-format",
+    "css-lsp",
+    "debugpy",
+    "html-lsp",
+    "isort",
+    "stylua",
+    "taplo",
+    "typescript-language-server",
+    "lua-language-server",
+    "markdownlint",
+    "marksman",
+    "prettier",
+    "pylint",
+    "basedpyright",
+    "shfmt",
+    "sql-formatter",
+    "yaml-language-server",
+    "biome",
+    "google-java-format",
+    "deno_fmt",
+    "taplo",
+    "markdownlint-cli2",
+    "markdown-toc",
+  },
 }
